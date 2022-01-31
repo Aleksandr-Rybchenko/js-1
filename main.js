873f18d3dcc0bdbf009312e3f656a5516c50ea6e
Aleksandr-Rybchenko <93516271+Aleksandr-Rybchenko@users.noreply.github.com>
2022-01-31 16:35:19 +0200

    Update main.js

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ if(age < 18) {
     alert('You are not allowed to visit this website')
 }
 if(age >= 22){
-        alert('Welcome' + " " + userName)
+        
 }
 if(age >= 18 && age <= 22)
     if (confirm("Are you sure you want to continue?")) {
